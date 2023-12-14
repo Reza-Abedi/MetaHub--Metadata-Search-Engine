3d4ffccba1aec44f2ae6ad228462b7a74d4f53cf
MohammadReza Abedi <reza@abedi.net>
2023-12-14 09:32:21 +0100

    ppt

--- extract-metadata-images.js
+++ extract-metadata-images.js
@@ -13,7 +13,10 @@ const pool = mysql.createPool({
 async function insertMetadata(image, metadata) {
   try {
     const connection = await pool.getConnection();
-    const [rows, fields] = await connection.execute('INSERT INTO Images (imageName, imageDescription) VALUES (?, ?)', [image, JSON.stringify(metadata)]);
+    const [rows, fields] = await connection.execute(
+      'INSERT INTO Images (imageName, imageDescription) VALUES (?, ?)',
+      [image, JSON.stringify(metadata)]
+    );
     console.log(`Metadata for ${image} inserted into the database.`);
     connection.release();
   } catch (error) {
@@ -21,20 +24,27 @@ async function insertMetadata(image, metadata) {
   }
 }
 
-let images = fs.readdirSync('Image');
-
-for (let image of images) {
-  if (image.slice(-4) === '.jpg') {
-    console.log('IMAGE: ' + image);
-    try {
-      let metadata = await exifr.parse('Image/' + image);
-      console.log(metadata);
-
-      await insertMetadata(image, metadata);
-    } catch (error) {
-      console.error(`Error processing metadata for ${image}: ${error.message}`);
+async function processImages() {
+  try {
+    let images = fs.readdirSync('Image');
+    for (let image of images) {
+      if (image.slice(-4) === '.jpg') {
+        console.log('IMAGE: ' + image);
+        try {
+          let metadata = await exifr.parse('Image/' + image);
+          console.log(metadata);
+          await insertMetadata(image, metadata);
+        } catch (error) {
+          console.error(`Error processing metadata for ${image}: ${error.message}`);
+        }
+      }
     }
+  } catch (error) {
+    console.error(`Error reading directory: ${error.message}`);
+  } finally {
+    pool.end();
   }
 }
 
-pool.end();
+// Call the function to start processing images
+processImages();
