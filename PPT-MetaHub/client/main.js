033fbf606fd143eb6e1f12c03ee5893f1811308d
MohammadReza Abedi <reza@abedi.net>
2023-12-21 12:19:13 +0100

    update mappar

--- PPT-MetaHub/client/main.js
+++ PPT-MetaHub/client/main.js
@@ -5,28 +5,51 @@ async function search() {
   // Read the search term from the input field
   let searchTerm = document.forms.searchForm.term.value;
 
+  // Read the selected sorting option
+  let titleSortOption = document.forms.searchForm.titleSort.value;
+
   // Make a fetch request to the server API endpoint for searching PowerPoint presentations
-  let response = await fetch(`/api/powerpoints/${searchTerm}`);
+  let response = await fetch(`/api/powerpoints/${searchTerm}?titleSort=${titleSortOption}`);
   let presentations = await response.json();
 
-  // Display the information in the HTML
+  // Display the information in the HTML table
   let resultsContainer = document.querySelector('.searchResults');
   resultsContainer.innerHTML = ''; // Clear previous results
 
-  presentations.forEach(presentation => {
-    let html = `
-      <section>
-        <h2>${presentation.title}</h2>
-        <p><b>Company:</b> ${presentation.company}</p>
-        <p><b>Timestamp:</b> ${presentation.timestamp}</p>
-        <p><b>Word Count:</b> ${presentation.wordCount}</p>
-        <p><b>Slide Count:</b> ${presentation.slideCount}</p>
-        <p><b>Creation Date:</b> ${presentation.creationDate}</p>
-        <p><b>Last Modified:</b> ${presentation.lastModified}</p>
-        <a href="${presentation.downloadLink}" download>Download File</a>
-      </section>
+  if (presentations.length > 0) {
+    let table = document.createElement('table');
+    table.classList.add('responsive-table');
+
+    table.innerHTML = `
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Company</th>
+          <th>Timestamp</th>
+          <th>Word Count</th>
+          <th>Creation Date</th>
+          <th>Last Modified</th>
+          <th>Download</th>
+        </tr>
+      </thead>
+      <tbody></tbody>
     `;
 
-    resultsContainer.innerHTML += html;
-  });
+    presentations.forEach(presentation => {
+      let row = table.querySelector('tbody').insertRow();
+      row.innerHTML = `
+        <td>${presentation.title}</td>
+        <td>${presentation.company}</td>
+        <td>${presentation.timestamp}</td>
+        <td>${presentation.wordCount}</td>
+        <td>${presentation.creationDate}</td>
+        <td>${presentation.lastModified}</td>
+        <td><a href="${presentation.downloadLink}" download>Download File</a></td>
+      `;
+    });
+
+    resultsContainer.appendChild(table);
+  } else {
+    resultsContainer.innerHTML = 'No results found.';
+  }
 }
--- PPT-MetaHub/server.js
+++ PPT-MetaHub/server.js
@@ -1,3 +1,5 @@
+// server.js
+
 import express from 'express';
 import mysql from 'mysql2/promise';
 
@@ -29,17 +31,24 @@ async function initializeDatabase() {
   // For example, you might want to create tables or perform other setup tasks
 }
 
-// API route for searching PowerPoint presentations by title
+// API route for searching PowerPoint presentations with advanced search options
 app.get('/api/powerpoints/:searchTerm', async (request, response) => {
   let searchTerm = request.params.searchTerm;
+  let titleSortOption = request.query.titleSort;
 
-  // Perform the database query and retrieve presentations based on the search term
-  // Replace the following lines with your actual database query logic
+  // Build the SQL query based on advanced search options
   let sql = `
     SELECT *
     FROM PowerPoints
     WHERE LOWER(pptDescription -> '$.title') LIKE LOWER (?)
   `;
+
+  // Add sorting based on title
+  if (titleSortOption === 'ascending') {
+    sql += ` ORDER BY pptDescription -> '$.title' ASC `;
+  } else if (titleSortOption === 'descending') {
+    sql += ` ORDER BY pptDescription -> '$.title' DESC `;
+  }
 
   let presentations = await query(sql, ['%' + searchTerm + '%']);
 
@@ -51,7 +60,7 @@ app.get('/api/powerpoints/:searchTerm', async (request, response) => {
       company: presentation.pptDescription.company || 'N/A',
       timestamp: presentation.pptDescription.timestamp || 'N/A',
       wordCount: presentation.pptDescription.word_count || 'N/A',
-      slideCount: presentation.pptDescription.slide_count || 'N/A',
+      slideCount: presentation.pptDescription.actual_slide_count_column_name || 'N/A',
       creationDate: presentation.pptDescription.creation_date || 'N/A',
       lastModified: presentation.pptDescription.last_modified || 'N/A',
       downloadLink: `/api/powerpoints/download/${presentation.pptName}` // Add download link
@@ -64,7 +73,7 @@ app.get('/api/powerpoints/:searchTerm', async (request, response) => {
 // API route for downloading PowerPoint presentations
 app.get('/api/powerpoints/download/:pptName', (request, response) => {
   // Replace the following line with logic to serve the file for download
-  const filePath = `C:/Users/Mreza.Abedi/OneDrive/Skrivbord/PowerpointsMetadata/client/ppt/${request.params.pptName}`;
+  const filePath = `C:/Users/Mreza.Abedi/OneDrive/Skrivbord/ppt-Metadata/client/ppt/${request.params.pptName}`;
   response.download(filePath);
 });
 
